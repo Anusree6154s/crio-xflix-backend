6087dbaa1ff37a81318445f4ff08225bba5db7fe
Anusree <anilkumaranusree113@gmail.com>
2024-11-15 19:23:53 +0530

    added frontend url to cors origin

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const videoRoutes = require("./server");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.NODE_ENV;
 
+app.use(cors({ origin: "https://xflix-frontend-hazel.vercel.app/" }));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.redirect("/v1/videos");
@@ -13,7 +15,7 @@ app.get("/", (req, res) => {
 app.use("/v1/videos", videoRoutes);
 
 mongoose
- // .connect("mongodb://127.0.0.1:27017/xflix")
+  // .connect("mongodb://127.0.0.1:27017/xflix")
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
